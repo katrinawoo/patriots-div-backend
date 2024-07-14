d5744570668bb7927357c81a6da0043febaf26f2
katrinawoo <katrinawoo@gmail.com>
2024-07-14 00:29:13 -0400

    Defined and run the migration for contact_us.

--- migrations/20240714042650_create_contact_messages.js
+++ migrations/20240714042650_create_contact_messages.js
@@ -2,14 +2,20 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+export function up(knex) {
+    return knex.schema.createTable('contact_messages', function(table) {
+      table.increments('id').primary();
+      table.string('name').notNullable();
+      table.string('email').notNullable();
+      table.text('message').notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+    });
+  }
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+export function down(knex) {
+    return knex.schema.dropTable('contact_messages');
+  }
