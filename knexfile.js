9dff5180696e2cdb38b8fac2b138bafe1d2ce203
katrinawoo <katrinawoo@gmail.com>
2024-07-16 06:23:58 -0400

    Accidentally deleted knexfile.js and uploads folder so added them back in.

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log('Database Config:', {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+export default {
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    charset: 'utf8'
+  },
+  migrations: {
+    directory: './migrations'
+  },
+  seeds: {
+    directory: './seeds'
+  }
+};
